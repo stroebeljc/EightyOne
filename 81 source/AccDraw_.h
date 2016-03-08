a7b0c4c5fb464f0de180102d9fae78b1fc16e30c
charlierobson <retrogit@gmail.com>
2016-03-08 11:37:33 +0000

    V0.43d

--- 81 source/AccDraw_.h
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------
-
-#ifndef AccDraw_H
-#define AccDraw_H
-//---------------------------------------------------------------------------
-#endif
-
-extern void AccurateInit(void);
-extern int AccurateDraw(void);

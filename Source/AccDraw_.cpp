177b4f6bce9b2559de9c3a059a483fc56151232a
ZXFruitcake <github@fruitcake.plus.com>
2021-09-12 01:15:01 +0100

    Added facility to create a blank microdrive file

--- Source/AccDraw_.cpp
+++ Source/AccDraw_.cpp
@@ -563,14 +563,13 @@ void GDIAccurateInit(int resize)
 void GDIAccurateUpdateDisplay(bool singlestep)
 {
         static int framecounter=0;
-        int ret;
-
+        
         if (++framecounter > zx81.frameskip || singlestep)
                 framecounter=0;
         else
                 return;
 
-        ret=StretchBlt(Form1->Canvas->Handle,
+        StretchBlt(Form1->Canvas->Handle,
                         rcdest.Left, rcdest.Top,
                         (rcdest.Right-rcdest.Left),
                         (rcdest.Bottom-rcdest.Top),

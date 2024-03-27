c0caf59b032f5b8e5d0d77ed55f4ebf260737b13
John Stroebel <stroej1@medtronic.com>
2024-03-27 09:45:54 -0500

    Included the Components folder for required Delphi component updates

---
+++ Components/ComPort Library/CPortTrmSet.hpp
@@ -0,0 +1,81 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2022 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'CPortTrmSet.pas' rev: 35.00 (Windows)
+
+#ifndef CporttrmsetHPP
+#define CporttrmsetHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+#include <System.hpp>
+#include <SysInit.hpp>
+#include <Winapi.Windows.hpp>
+#include <Winapi.Messages.hpp>
+#include <System.SysUtils.hpp>
+#include <System.Classes.hpp>
+#include <Vcl.Graphics.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <CPortCtl.hpp>
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Cporttrmset
+{
+//-- forward type declarations -----------------------------------------------
+class DELPHICLASS TComTrmSetForm;
+//-- type declarations -------------------------------------------------------
+class PASCALIMPLEMENTATION TComTrmSetForm : public Vcl::Forms::TForm
+{
+	typedef Vcl::Forms::TForm inherited;
+	
+__published:
+	Vcl::Stdctrls::TGroupBox* GroupBox1;
+	Vcl::Stdctrls::TCheckBox* CheckBox1;
+	Vcl::Stdctrls::TCheckBox* CheckBox2;
+	Vcl::Stdctrls::TCheckBox* CheckBox3;
+	Vcl::Stdctrls::TCheckBox* CheckBox4;
+	Vcl::Stdctrls::TCheckBox* CheckBox5;
+	Vcl::Stdctrls::TGroupBox* GroupBox2;
+	Vcl::Stdctrls::TLabel* Label1;
+	Vcl::Stdctrls::TComboBox* ComboBox1;
+	Vcl::Stdctrls::TLabel* Label3;
+	Vcl::Stdctrls::TLabel* Label4;
+	Vcl::Stdctrls::TComboBox* ComboBox2;
+	Vcl::Stdctrls::TLabel* Label2;
+	Vcl::Stdctrls::TComboBox* ComboBox3;
+	Vcl::Stdctrls::TLabel* Label5;
+	Vcl::Stdctrls::TButton* Button1;
+	Vcl::Stdctrls::TButton* Button2;
+	Vcl::Stdctrls::TEdit* Edit1;
+	Vcl::Stdctrls::TEdit* Edit2;
+public:
+	/* TCustomForm.Create */ inline __fastcall virtual TComTrmSetForm(System::Classes::TComponent* AOwner) : Vcl::Forms::TForm(AOwner) { }
+	/* TCustomForm.CreateNew */ inline __fastcall virtual TComTrmSetForm(System::Classes::TComponent* AOwner, int Dummy) : Vcl::Forms::TForm(AOwner, Dummy) { }
+	/* TCustomForm.Destroy */ inline __fastcall virtual ~TComTrmSetForm() { }
+	
+public:
+	/* TWinControl.CreateParented */ inline __fastcall TComTrmSetForm(HWND ParentWindow) : Vcl::Forms::TForm(ParentWindow) { }
+	
+};
+
+
+//-- var, const, procedure ---------------------------------------------------
+extern DELPHI_PACKAGE void __fastcall EditComTerminal(Cportctl::TCustomComTerminal* ComTerminal);
+}	/* namespace Cporttrmset */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_CPORTTRMSET)
+using namespace Cporttrmset;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// CporttrmsetHPP
---
+++ Components/Office Button 97/OffBtn97/OffBtn.hpp
@@ -0,0 +1,81 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2022 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'OffBtn.dpk' rev: 35.00 (Windows)
+
+#ifndef OffbtnHPP
+#define OffbtnHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+#include <System.hpp>	// (rtl)
+#include <SysInit.hpp>
+#include <OffBtn.hpp>
+#include <System.UITypes.hpp>	// (rtl)
+#include <Winapi.Windows.hpp>	// (rtl)
+#include <Winapi.PsAPI.hpp>	// (rtl)
+#include <System.Character.hpp>	// (rtl)
+#include <System.Internal.ExcUtils.hpp>	// (rtl)
+#include <System.SysUtils.hpp>	// (rtl)
+#include <System.VarUtils.hpp>	// (rtl)
+#include <System.Variants.hpp>	// (rtl)
+#include <System.TypInfo.hpp>	// (rtl)
+#include <System.Math.hpp>	// (rtl)
+#include <System.Generics.Defaults.hpp>	// (rtl)
+#include <System.Rtti.hpp>	// (rtl)
+#include <System.TimeSpan.hpp>	// (rtl)
+#include <System.Classes.hpp>	// (rtl)
+#include <Winapi.ShellAPI.hpp>	// (rtl)
+#include <System.DateUtils.hpp>	// (rtl)
+#include <System.IOUtils.hpp>	// (rtl)
+#include <System.IniFiles.hpp>	// (rtl)
+#include <System.Win.Registry.hpp>	// (rtl)
+#include <System.UIConsts.hpp>	// (rtl)
+#include <Vcl.Graphics.hpp>	// (vcl)
+#include <System.Messaging.hpp>	// (rtl)
+#include <System.Actions.hpp>	// (rtl)
+#include <Vcl.ActnList.hpp>	// (vcl)
+#include <System.HelpIntfs.hpp>	// (rtl)
+#include <System.SyncObjs.hpp>	// (rtl)
+#include <Winapi.UxTheme.hpp>	// (rtl)
+#include <Vcl.GraphUtil.hpp>	// (vcl)
+#include <Vcl.StdCtrls.hpp>	// (vcl)
+#include <Vcl.Clipbrd.hpp>	// (vcl)
+#include <Vcl.Printers.hpp>	// (vcl)
+#include <Vcl.ComCtrls.hpp>	// (vcl)
+#include <Vcl.Dialogs.hpp>	// (vcl)
+#include <Vcl.ExtCtrls.hpp>	// (vcl)
+#include <Vcl.Themes.hpp>	// (vcl)
+#include <System.AnsiStrings.hpp>	// (rtl)
+#include <System.Win.ComObj.hpp>	// (rtl)
+#include <Winapi.FlatSB.hpp>	// (rtl)
+#include <Vcl.Forms.hpp>	// (vcl)
+#include <Vcl.Menus.hpp>	// (vcl)
+#include <Winapi.MsCTF.hpp>	// (rtl)
+#include <Vcl.Controls.hpp>	// (vcl)
+// PRG_EXT: .bpl
+// BPI_DIR: C:\EightyOne_Mig\Components\Office Button 97\OffBtn97
+// OBJ_DIR: C:\EightyOne_Mig\Components\Office Button 97\OffBtn97
+// OBJ_EXT: .obj
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Offbtn
+{
+//-- forward type declarations -----------------------------------------------
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+}	/* namespace Offbtn */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_OFFBTN)
+using namespace Offbtn;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// OffbtnHPP

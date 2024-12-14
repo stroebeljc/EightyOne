b4fb7684ffde70db86f6996da07be4f209ea0b98
John Stroebel <35274651+stroebeljc@users.noreply.github.com>
2024-12-14 17:17:10 -0600

    Removed code changed comment

--- Source/zx81config.h
+++ Source/zx81config.h
@@ -188,7 +188,7 @@ typedef struct
         CFGBYTE chrgen;
         CFGBYTE enableQSchrgen;
         CFGBYTE vsyncsound;
-        CFGBYTE parrot; // Stroebel modified
+        CFGBYTE parrot;
         int maxireg;
         CFGBYTE zxpand;
         int RAMTOP;

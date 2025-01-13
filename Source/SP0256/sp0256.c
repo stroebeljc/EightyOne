66d6f30b6e5cc9425e80e16ef2606b9d95b14e33
John Stroebel <stroej1@medtronic.com>
2025-01-13 16:11:44 -0600

    Disable debug print statements in SP0256 emulator

--- Source/SP0256/sp0256.c
+++ Source/SP0256/sp0256.c
@@ -51,7 +51,7 @@
 
 static int s_debugSample = 0;
 
-#if 1
+#if 0
 #define dsprintf(x) if( s_debugSample ) { jzp_printf x ; jzp_flush(); }
 #else
 #undef DEBUG_SAMPLE
@@ -65,11 +65,11 @@ static int s_debugSample = 0;
 
 static int s_debug = 0;
 
-#if 1
+#if 0
 #define jzdprintf(x) if( s_debug ) { jzp_printf x ; jzp_flush(); }
 #else
 #undef DEBUG
-#define DEBUG
+//#define DEBUG
 #ifdef DEBUG
 #define jzdprintf(x) jzp_printf x ; jzp_flush()
 #else

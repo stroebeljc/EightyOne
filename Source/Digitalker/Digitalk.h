a664575d6437dc86d479a2d2c3e891256c6749d1
John Stroebel <stroej1@medtronic.com>
2025-01-28 11:00:10 -0600

    Reverted Digitalker to original MAME data types to prevent stalls due to unexpected negatives

--- Source/Digitalker/Digitalk.h
+++ Source/Digitalker/Digitalk.h
@@ -7,6 +7,7 @@
 
 #include <stdlib.h>
 #include <cstring>
+#include "types.h"
 
 //**************************************************************************
 //  TYPE DEFINITIONS
@@ -19,18 +20,18 @@ class digitalker_device
 public:
 	digitalker_device();
 
-	void digitalker_start_command(int cmd);
+	void digitalker_start_command(uint8_t cmd);
 	int digitalker_0_intr_r();
         int single_sample(void);
-        void setROM(unsigned char *romPtr);
+        void setROM(uint8_t *romPtr);
 
 	// device-level overrides
 	void device_start();
 
 private:
-	void digitalker_write(int *adr, int vol, int dac);
-	int digitalker_pitch_next(int val, int prev, int step);
-	void digitalker_set_intr(int intr);
+	void digitalker_write(uint8_t *adr, uint8_t vol, int8_t dac);
+	uint8_t digitalker_pitch_next(uint8_t val, uint8_t prev, int step);
+	void digitalker_set_intr(uint8_t intr);
 	void digitalker_step_mode_0();
 	void digitalker_step_mode_1();
 	void digitalker_step_mode_2();
@@ -39,35 +40,35 @@ private:
 	int digitalker_intr_r();
 
 	// Port/lines state
-	int m_data;
-	int m_intr;
+	uint8_t m_data;
+	uint8_t m_intr;
 
 	// Current decoding state
-	int m_bpos;
-	int m_apos;
+	uint16_t m_bpos;
+	uint16_t m_apos;
 
-	int m_mode;
-	int m_cur_segment;
-	int m_cur_repeat;
-	int m_segments;
-	int m_repeats;
+	uint8_t m_mode;
+	uint8_t m_cur_segment;
+	uint8_t m_cur_repeat;
+	uint8_t m_segments;
+	uint8_t m_repeats;
 
-	int m_prev_pitch;
-	int m_pitch;
-	int m_pitch_pos;
+	uint8_t m_prev_pitch;
+	uint8_t m_pitch;
+	uint8_t m_pitch_pos;
 
-	int m_stop_after;
-	int m_cur_dac;
-	int m_cur_bits;
+	uint8_t m_stop_after;
+	uint8_t m_cur_dac;
+	uint8_t m_cur_bits;
 
 	// Zero-range size
-	unsigned int m_zero_count; // 0 for done
+	uint32_t m_zero_count; // 0 for done
 
 	// Waveform and current index in it
-	int m_dac_index; // 128 for done
-	int m_dac[128];
+	uint8_t m_dac_index; // 128 for done
+	int16_t m_dac[128];
 
-        unsigned char *m_rom;
+        uint8_t *m_rom;
 };
 
 #endif // MAME_SOUND_DIGITALK_H

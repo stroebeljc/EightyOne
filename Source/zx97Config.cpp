a228f3f78ac1ce1ae8b596881e85e844249714e2
John Stroebel <stroej1@medtronic.com>
2025-02-12 15:40:37 -0600

    Addressed issues between ZX97 form and hardware form

--- Source/zx97Config.cpp
+++ Source/zx97Config.cpp
@@ -32,6 +32,7 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TZX97Dialog *ZX97Dialog;
+unsigned char zx97bankmem[16*16384];
 
 //---------------------------------------------------------------------------
 __fastcall TZX97Dialog::TZX97Dialog(TComponent* Owner)
@@ -39,21 +40,19 @@ __fastcall TZX97Dialog::TZX97Dialog(TComponent* Owner)
 {
         TIniFile *ini;
 
-        SaveToInternalSettings(); // save in case there is no INI file
         ini = new TIniFile(emulator.inipath);
         LoadSettings(ini);
         delete ini;
 }
 //---------------------------------------------------------------------------
-void TZX97Dialog::UpdateRequired(void)
+void TZX97Dialog::UpdateMachine(ZX97& settings)
 {
-        SaveToInternalSettings(); // save copy to keep user choices
-        zx97.saveram=Zx97form.saveram;
-        zx97.protectab=Zx97form.protectab;
-        zx97.protect08=Zx97form.protect08;
-        zx97.protectb0=Zx97form.protectb0;
-        zx97.protectb115=Zx97form.protectb115;
-        zx97.bankswitch=Zx97form.bankswitch;
+        zx97.saveram=settings.saveram;
+        zx97.protectab=settings.protectab;
+        zx97.protect08=settings.protect08;
+        zx97.protectb0=settings.protectb0;
+        zx97.protectb115=settings.protectb115;
+        zx97.bankswitch=settings.bankswitch;
 }
 
 void TZX97Dialog::SaveSettings(TIniFile *ini)
@@ -70,7 +69,7 @@ void TZX97Dialog::SaveSettings(TIniFile *ini)
                 f=fopen(filename,"wb");
                 if (f)
                 {
-                        fwrite(Zx97form.bankmem, 16384, 16, f);
+                        fwrite(zx97bankmem, 16384, 16, f);
                         fclose(f);
                 }
         }
@@ -90,28 +89,30 @@ void TZX97Dialog::LoadSettings(TIniFile *ini)
         f=fopen(filename,"rb");
         if (f)
         {
-                fread(Zx97form.bankmem, 16384, 16, f);
+                fread(zx97bankmem, 16384, 16, f);
                 fclose(f);
         }
 
         AccessIniFile(ini, Read);
-        LoadFromInternalSettings(); // restore form settings from copy
 }
 
 void TZX97Dialog::AccessIniFile(TIniFile* ini, IniFileAccessType accessType)
 {
         AccessIniFileInteger(ini, accessType, "ZX97", "Top",   Top);
         AccessIniFileInteger(ini, accessType, "ZX97", "Left",  Left);
-
-        AccessIniFileInteger(ini, accessType, "ZX97", "SaveRAM",    Zx97form.saveram);
-        AccessIniFileInteger(ini, accessType, "ZX97", "08k",        Zx97form.protect08);
-        AccessIniFileInteger(ini, accessType, "ZX97", "abk",        Zx97form.protectab);
-        AccessIniFileInteger(ini, accessType, "ZX97", "b0",         Zx97form.protectb0);
-        AccessIniFileInteger(ini, accessType, "ZX97", "b115",       Zx97form.protectb115);
-        AccessIniFileInteger(ini, accessType, "ZX97", "SwapRAMROM", Zx97form.bankswitch);
 }
 
 //---------------------------------------------------------------------------
+
+void TZX97Dialog::RetrieveFormSettings(ZX97& settings)
+{
+        settings.saveram=Zx97form.saveram;
+        settings.protectab=Zx97form.protectab;
+        settings.protect08=Zx97form.protect08;
+        settings.protectb0=Zx97form.protectb0;
+        settings.protectb115=Zx97form.protectb115;
+        settings.bankswitch=Zx97form.bankswitch;
+}
 
 void TZX97Dialog::SaveToInternalSettings()
 {
@@ -121,6 +122,17 @@ void TZX97Dialog::SaveToInternalSettings()
         Zx97form.protectb0=ProtectB0->Checked;
         Zx97form.protectb115=ProtectB115->Checked;
         Zx97form.bankswitch=(SwapRAMROM->Checked!=0);
+}
+
+void TZX97Dialog::UpdateFormSettings(ZX97& settings)
+{
+        Zx97form.saveram=settings.saveram;
+        Zx97form.protectab=settings.protectab;
+        Zx97form.protect08=settings.protect08;
+        Zx97form.protectb0=settings.protectb0;
+        Zx97form.protectb115=settings.protectb115;
+        Zx97form.bankswitch=settings.bankswitch;
+        LoadFromInternalSettings();
 }
 
 void TZX97Dialog::LoadFromInternalSettings()
@@ -133,15 +145,15 @@ void TZX97Dialog::LoadFromInternalSettings()
         SwapRAMROM->Checked=Zx97form.bankswitch;
 }
 
-void __fastcall TZX97Dialog::FormShow(TObject *Sender)
+void __fastcall TZX97Dialog::OKClick(TObject *Sender)
 {
-        LoadFromInternalSettings();  // restore form settings from copy
+        SaveToInternalSettings();
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TZX97Dialog::CancelClick(TObject *Sender)
 {
-        LoadFromInternalSettings(); // restore form settings from copy
+        LoadFromInternalSettings();
 }
 //---------------------------------------------------------------------------
 

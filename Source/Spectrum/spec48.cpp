6d06d08ba22922facc839a6429f78a4c1414ffdd
John Stroebel <35274651+stroebeljc@users.noreply.github.com>
2026-04-27 16:33:57 -0500

    Fixed CPU operand read method and moved RZX counter out of CPU processing

--- Source/Spectrum/spec48.cpp
+++ Source/Spectrum/spec48.cpp
@@ -154,7 +154,7 @@ BOOL insertWaitsWhileSP0256Busy;
 
 extern AnsiString AdjustPathIfReplacementRom(char* curRom);
 
-extern unsigned short RZXCounter;
+unsigned short RZXCounter;
 extern RZX_INFO rzx;
 
 int TIMEXByte, TIMEXMode, TIMEXColour;
@@ -938,6 +938,7 @@ BYTE spec48_readoperandbyte(int Address)
 // Called by Z80 instruction opcode fetches
 BYTE spec48_opcode_fetch(int Address)
 {
+        RZXCounter--;
         return(spec48_ReadByte(Address));
 }
 
